c0a65d7a2dec498146e841ff4516837c59b61c29
aggie97 <4ggie1997@gmail.com>
2023-07-27 11:05:21 +0900

    ⚡️ [Feature]: useCart hook 적용
    상품 목록 페이지의 기존 로직 useCart hook으로 대체

--- src/components/units/product/list/Item/index.tsx
+++ src/components/units/product/list/Item/index.tsx
@@ -1,46 +1,26 @@
-import type { MouseEvent } from 'react';
-import type { Product } from 'src/types/types';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { type MouseEvent } from 'react';
+
+import type { Product } from 'src/types/types';
 import ItemCard from 'src/components/common/itemCard';
 import CartButton from 'src/components/common/cartButton';
-import { cartListState, cartListStatsState } from 'src/store/store';
+import { TEST_BLUR_URL } from 'src/constants/url';
+import useCart from 'src/hooks/useCart';
 
 interface ItemProps {
   data: Product;
 }
 
-const TEST_BLUR_URL =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOcYAwAAVkAxbGRRlAAAAAASUVORK5CYII=';
-
 const Item = ({ data }: ItemProps) => {
-  const [cartList, setCartList] = useRecoilState(cartListState);
-  const { totalNum } = useRecoilValue(cartListStatsState);
+  const { checkItemInCart, setItemToCart, deleteItemFromCart } = useCart();
 
-  const isInCart = cartList.some((product) => product.item_no === data.item_no);
-
+  const isExistInCart = checkItemInCart(data.item_no);
   const onClickButton = (event: MouseEvent<HTMLButtonElement>) => {
-    // event.stopPropagation();
-    // stopPropagation() 으로 a 태그가 클릭되는 것을 막지 못함.
-    // Link 태그는 본래 client-side로 작동하지만, 다른 이벤트에서 전파되어 클릭되면
-    // server-side로 작동하여 페이지가 새로고침 된다.
     event.preventDefault();
-    // 장바구니에
-    if (isInCart) {
-      // 있다면 빼기
-      setCartList((oldCartList) =>
-        oldCartList.filter((product) => product.item_no !== data.item_no)
-      );
-    } else {
-      // 없다면 담기
-      // 장바구니 개수 제한(3개)
-      if (totalNum && totalNum > 2) {
-        alert('장바구니가 가득찼습니다.');
-        return;
-      }
-      setCartList((oldCartList) => [...oldCartList, { ...data, count: 1, checked: false }]);
-    }
+
+    if (!isExistInCart) setItemToCart(data);
+    else deleteItemFromCart([data]);
   };
   return (
     <ItemCard>
@@ -57,7 +37,7 @@ const Item = ({ data }: ItemProps) => {
         <span>{data.item_name}</span>
         <div className="footer">
           <span>{data.price.toLocaleString()}원</span>
-          <CartButton mode="toggle" onClickButton={onClickButton} isInCart={isInCart} />
+          <CartButton mode="toggle" onClickButton={onClickButton} isInCart={isExistInCart} />
         </div>
       </Link>
     </ItemCard>
